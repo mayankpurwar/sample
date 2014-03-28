28a595d98b3c9c3ce4036cc1b2ca17503364949c
mayankpurwar <apple@Mayanks-Mac-mini.local>
2014-03-28 17:59:48 +0530

    changes code for replacing tab
    changes code for replacing tab

--- tabbedtest/tabbedtest/FirstViewController.m
+++ tabbedtest/tabbedtest/FirstViewController.m
@@ -37,81 +37,15 @@
 
 -(IBAction)ClickedBtn
 {
-//    [[[[self.tabBarController tabBar]items]objectAtIndex:1]setEnabled:FALSE];
-    
-//     NSLog(@" %lu",(unsigned long)[[self.tabBarController tabBar]items].count);
-//    NSArray *arr = [[self.tabBarController tabBar]items];
-//    NSLog(@"%@",arr);
-//   
-//    [self.tabBarController.tabBar setItems:[NSArray arrayWithObjects:[arr objectAtIndex:0],[arr objectAtIndex:1], nil]];
-////    [self.tabBarController setViewControllers:[NSArray arrayWithObjects:[arr objectAtIndex:0],[arr objectAtIndex:1], nil] animated:YES];
-//    
-////    [(UITabBarController*)self.navigationController.topViewController setSelectedIndex:3];
-//
-//    
-//    
+
      NSLog(@" %lu",(unsigned long)[[self.tabBarController tabBar]items].count);
-//
-    
-    
-    
-    
-    
-    
-//    if ([self userDataIsValid])
-    {
-//        NSMutableArray* newArray = [NSMutableArray arrayWithArray:self.tabBarController.viewControllers];
-        
-        
+
         ViewController *vc = [[ViewController alloc] init];
-//        [newArray replaceObjectAtIndex:0 withObject:vc];
-//
-//        [self.tabBarController setViewControllers:newArray animated:YES];
-//        [self.tabBarController.tabBarController setSelectedIndex:1];
-        
-        
         NSMutableArray *viewControllersCopy = [[self.tabBarController viewControllers] mutableCopy];
         [viewControllersCopy replaceObjectAtIndex:0 withObject:vc];
         NSArray *modifiedViewControllers = [[NSArray alloc] initWithArray:viewControllersCopy];
         [self.tabBarController setViewControllers:modifiedViewControllers animated:NO];
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        
-//        UIBarItem *itemToRemove = nil;
-//        
-//        // have we been purchased?
-//            // see if purchase view controller is in the tab bar...
-//            NSMutableArray *newItems = [NSMutableArray arrayWithArray:self.tabBarController.tabBar.items];
-//            for (UIBarItem *tabItem in newItems)
-//            {
-//                if ([self.tabBarItem.title isEqualToString:@"Item"])
-//                {
-//                    //store this item to remove later
-//                    itemToRemove = tabItem;
-//                }
-//            }
-//            if (itemToRemove)
-//            {
-//                [newItems removeObject:itemToRemove];
-//                [self.tabBarController.tabBar setItems:newItems animated:NO];
-//            }
-//        
-//        
-        
-        
-        
-        
-    }
+
 }
 
     
